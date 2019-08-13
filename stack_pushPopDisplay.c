8b5815fa49bb89be560ec9704632bf2ff4eca4c5
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2019-08-13 11:00:49 +0530

    Add files via upload

---
+++ stack_pushPopDisplay.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+void push(int a[100], int ele, int *z);
+void display(int a[100], int *z);
+int pop(int a[100], int *z);
+void main(){
+    int arr[100], choice, cont, size, i, z=-1;
+    cont=1;
+    printf("Enter the total number of elements - \n");
+    scanf("%d",&size);
+    for(i=0;i<size;i++){
+        printf("\nEnter element %d- ",i);
+        scanf("%d", &arr[i]);
+        z++;
+
+    }
+    do{
+        printf("\n Enter the operation number -\n1. Push\n2. Pop\n3. Display\n");
+        scanf("%d",&choice);
+        switch(choice){
+        case 1: {
+                printf("\nEnter the element to push to array-\n");
+                int b;
+                scanf("%d", &b);
+                push(arr,b,&z);
+                break;
+        }
+        case 2: {int x;
+                x=pop(arr, &z);
+                if(x!=-9999)
+                    printf("Popped element is %d\n", x);
+                break;
+        }
+        case 3: display(arr, &z);
+
+        }
+        printf("\nDo you want to repeat?\n(Enter 1 for yes, 0 for no)-");
+        scanf("%d", &cont);
+    }while(cont==1);
+
+}
+void push(int a[], int ele, int *z){
+    if(*z==99){
+        printf("Stack overflow\n");
+        return;
+    }
+    *z=*z+1;
+    a[*z]=ele;
+}
+void display(int a[], int *z){
+    if(*z==-1){
+        printf("Stack is empty\n");
+        return;
+    }
+    int i;
+    for(i=0;i<=*z;i++){
+        printf("%d\n",a[i]);
+    }
+}
+int pop(int a[], int *z)
+{
+    if(*z==-1){
+        printf("Stack underflow\n");
+        return -9999;
+    }
+    return a[(*z)--];
+}
