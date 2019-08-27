4a1c1c33ddeec7bb82994815480b379ac50c04ed
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2019-08-27 10:41:18 +0530

    Add files via upload

---
+++ fibo.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int fib(int a);
+int main(){
+	int n, i, a;
+	printf("Enter the Number of series -\n");
+ 	scanf("%d", &n);
+ 	for(i = 0 ; i < n ; i++){
+ 		a = fib(i);
+ 		printf("%d ", a);
+	}
+	printf("\n");
+	return 0;
+}
+int fib(int a){
+	int x, y;
+	if((a==0)){
+		return 0;
+	}
+	else if(a==1){
+		return 1;
+	}
+	else{
+		x = fib(a-1);
+  		y = fib(a-2);
+		return(x + y);
+	}
+}
+
