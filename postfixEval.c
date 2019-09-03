a9d1e043cb572bc083dc1343ded22b0b7f8b822a
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2019-09-03 10:40:34 +0530

    Add files via upload

--- postfixEval.c
+++ postfixEval.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<ctype.h>
 void push(int stack[], int *z, int a);
 int pop(int stack[], int *z);
 int res(int a, int b, char c);
@@ -7,18 +8,18 @@ int main(){
 	int opr1, opr2, ans, x;
 	char post[100];
 	printf("Enter the Postfix expression - ");
-	scanf("%s", &post);
+	scanf("%s", post);
 	while(post[i] != '\0'){
 		if(isdigit(post[i])){
 			x = (int)post[i] - 48;
 			push(s, &top, x);
-        	}
+        }
 		else{
 			opr2 = pop(s, &top);
 			opr1 = pop(s, &top);
 			value = res(opr1, opr2, post[i]);
 			push(s, &top, value);
-        	}
+        }
 		i++;
     	}
     	ans = pop(s, &top);
@@ -28,7 +29,7 @@ int main(){
 void push(int stack[], int *z, int a){
 	if (*z == 19){
 		printf("Stack Overflow\n");
-    	}
+   	}
 	*z += 1;
 	stack[*z] = a;
 }
@@ -36,12 +37,12 @@ int pop(int stack[], int *z){
 	if(*z == -1){
 		printf("Stack Underflow\n");
 		return 0;
-    	}
+    }
 	return stack[(*z)--];
 }
 int res(int a, int b, char c){
 	switch(c){
-        	case '+': return (a+b);
+        case '+': return (a+b);
 		case '-': return (a-b);
 		case '*': return (a*b);
 		case '/': return (a/b);
